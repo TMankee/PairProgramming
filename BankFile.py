42dff1c01a31f11c53dff8e08310e29dc6c4cca3
Damian Farrelly <damian.farrelly@atos.net>
2021-08-09 15:26:10 +0100

    withdraw & deposit
    I think both methods work but unsure

--- BankFile.py
+++ BankFile.py
@@ -9,4 +9,16 @@ class BankAccount:
         self.balance = c
 
     def deposit(self):
-        self.balance = self.balance + 
+        self.balance = self.balance + float(input("How much would you like to deposit? \n"))
+    
+    def withdraw(self):
+        amount = float(input("How much would you like to withdrawl? \n"))
+        if self.balance < amount:
+            print("Error")
+        else:
+            self.balance = self.balance - amount
+
+obj = BankAccount("Joe Bloggs",10000,100.00)
+
+obj.deposit()
+obj.withdraw()
